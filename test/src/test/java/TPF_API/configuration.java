7b412647573d166b5705d125cdf3f87b63e8e112
Abedlerahman Adam <abdoadam1511999@gmail.com>
2026-01-14 16:38:06 +0200

    restriction v2
    Signed-off-by: Abedlerahman Adam <abdoadam1511999@gmail.com>

--- test/src/test/java/TPF_API/configuration.java
+++
@@ -1,9 +0,0 @@
-package TPF_API;
-
-
-public class configuration {
-	public static final String DIRECTIONSR_STRING = "D:\\reconcilation mini\\script";
-	public static final String GALILEO_FILE_NAME_STRING = "Galileo Active Data.csv";
-	public static final String STAR_Date= "2025-12-30T00:00:00";
-	public static final String END_Date= "2025-12-31T00:00:00";
-}
